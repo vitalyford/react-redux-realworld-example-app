a4e88042f49822a394e07a395a5f815205e70a18
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2020-05-11 20:44:00 +0000

    Change header info

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -10,7 +10,7 @@ const Banner = ({ appName, token }) => {
         <h1 className="logo-font">
           {appName.toLowerCase()}
         </h1>
-        <p>A place to share your knowledge.</p>
+        <p>A real place in a real world to share real knowledge</p>
       </div>
     </div>
   );
